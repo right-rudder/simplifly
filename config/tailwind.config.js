275a808324c51984a53a598c36f7cbed91aedcfb
Tim Jedrek <tim@timjedrek.com>
2023-05-30 05:09:24 -0500

    first section

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -123,6 +123,8 @@ module.exports = {
         'pilot-thumbs' : "url('pilot_thumbs.webp')",
         'hangarbg' : "url('hangarbg.webp')",
         'mountain' : "url('mountain_backdrop.webp')",
+        'discovery-flight' : "url('discovery-flight-bg.webp')"
+        
         
       },
       height: theme => ({
