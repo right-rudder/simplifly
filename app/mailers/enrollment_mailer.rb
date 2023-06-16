4e89c96372e15b1a16e57dbee6d42e2b4f58c5bf
Tim Jedrek <tim@timjedrek.com>
2023-06-16 16:39:28 +0200

    configure mailer

--- app/mailers/enrollment_mailer.rb
+++ app/mailers/enrollment_mailer.rb
@@ -1,2 +1,12 @@
 class EnrollmentMailer < ApplicationMailer
+
+  default from: 'no-reply@simplifly-co.com'
+
+  def enrollment_email(message)
+    @message = message
+
+    mail(to: @message.email, bcc: ["no-reply@simplifly-co.com", ""], subject: "✈️ Application to Enroll at SimpliFly")
+  end
+
+
 end
