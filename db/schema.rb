77ab90eeda7a6e667b16fd0d8245ff796e8f840d
Tim Jedrek <tim@timjedrek.com>
2023-06-16 14:32:01 +0200

    add location to show for testing purposes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_16_120727) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_16_123024) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -107,6 +107,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_16_120727) do
     t.string "alternate_availability"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "location"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
