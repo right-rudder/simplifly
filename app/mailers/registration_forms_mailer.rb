56dd413fe5c7542f17db366d801c8d982c43dcbb
Tim Jedrek <tim@timjedrek.com>
2023-10-19 03:18:14 -0500

    try to get CSS to work

--- app/mailers/registration_forms_mailer.rb
+++ app/mailers/registration_forms_mailer.rb
@@ -1,11 +1,11 @@
 class RegistrationFormsMailer < ApplicationMailer
   def send_registration_form_pdf(registration_form)
+    @registration_form = registration_form
     pdf = WickedPdf.new.pdf_from_string(
       render_to_string(
-        pdf: 'registration_form',
-        template: 'registration_forms/pdf_template.html.erb',
-        #layout: 'pdf_layout.html.erb',
-        layout: false,  # This line excludes the layout
+        pdf: registration_form.first_name + ' ' + registration_form.last_name + ' Registration Form',
+        template: 'registration_forms_mailer/pdf_template',
+        layout: 'pdf_mailer',
         locals: { registration_form: registration_form }
       )
     )
