9902187848cdb4c68f1c8798874e5520bb9857ef
Tim Jedrek <tim@timjedrek.com>
2023-11-17 10:45:11 -0600

    add validation

--- app/models/reimbursement.rb
+++ app/models/reimbursement.rb
@@ -1,3 +1,9 @@
 class Reimbursement < ApplicationRecord
   has_one_attached :receipt 
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :entry_date, presence: true
+  validates :amount, presence: true
+  validates :preferred_payment, presence: true
 end
