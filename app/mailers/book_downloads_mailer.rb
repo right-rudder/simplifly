44b753988998b0834fcfa44a82048cab07baf0c0
Tim Jedrek <tim@timjedrek.com>
2024-02-05 16:01:02 -0600

    add book download to ghl integration

--- app/mailers/book_downloads_mailer.rb
+++ app/mailers/book_downloads_mailer.rb
@@ -5,8 +5,10 @@ class BookDownloadsMailer < ApplicationMailer
   def book_downloads(book_download)
     @book_download = book_download
 
-    mail(to: @book_download.email, bcc: ["info@simplifly-co.com", "info@simpliflyco.com"], subject: "✈️ SimpliFly FREE Pilot Training Booklet")
+    if Rails.env.production?
+      mail(to: @book_download.email, bcc: ["info@simplifly-co.com", "info@simpliflyco.com"], subject: "✈️ SimpliFly FREE Pilot Training Booklet")
+    else
+      mail(to: @book_download.email, bcc: ["info@simplifly-co.com"], subject: "✈️ SimpliFly FREE Pilot Training Booklet")
+    end
   end
-
-
 end
