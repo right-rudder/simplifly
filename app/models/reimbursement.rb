f3c6380b10f937072c165920a3196fd516dc170f
Raul Ospina <raminka13@hotmail.com>
2024-04-30 20:46:33 -0500

    Update: Refactor image resizing logic RMAGICK

--- app/models/reimbursement.rb
+++ app/models/reimbursement.rb
@@ -1,7 +1,7 @@
 class Reimbursement < ApplicationRecord
-  #require "mini_magick"
+  require "rmagick"
 
-  has_one_attached :receipt 
+  has_one_attached :receipt
 
   validates :first_name, presence: true
   validates :last_name, presence: true
@@ -12,26 +12,25 @@ class Reimbursement < ApplicationRecord
   validates :receipt, content_type: ['image/png', 'image/gif', 'image/jpeg', 'image/jpg'],
   size: { less_than: 2.megabytes, message: 'File size must be less than 2MB' }
 
-#  before_save :resize_image, if: -> { receipt.attached? }
-#
-#  private
-#
-#  def resize_image
-#    max_dimension = 800 # Adjust the maximum dimension as needed
-#
-#    # Create a MiniMagick::Image directly from the binary data
-#    image = MiniMagick::Image.create(receipt.blob)
-#    image.resize "#{max_dimension}x#{max_dimension}>"
-#    
-#    # Create a new ActiveStorage::Blob with the resized image data
-#    resized_image_blob = ActiveStorage::Blob.create_after_upload!(
-#      io: StringIO.new(image.to_blob),
-#      filename: receipt.filename,
-#      content_type: image.mime_type
-#    )
-#
-#    # Replace the original attachment with the resized image
-#    receipt.attach(resized_image_blob)
-#  end
+  before_save :resize_image, if: -> { receipt.attached? }
+
+  private
+
+  def resize_image
+    max_dimension = 800 # Adjust the maximum dimension as needed
+    # Create a MiniMagick::Image directly from the binary data
+    image = Magick::Image::read(receipt.download).first
+    image.resize_to_fill "#{max_dimension}x#{max_dimension}>"
+
+    Create a new ActiveStorage::Blob with the resized image data
+    resized_image_blob = ActiveStorage::Blob.create_after_upload!(
+      io: StringIO.new(image.to_blob),
+      filename: receipt.filename,
+      content_type: image.mime_type
+      )
+
+    # Replace the original attachment with the resized image
+    receipt.attach(resized_image_blob)
+    end
 
 end
