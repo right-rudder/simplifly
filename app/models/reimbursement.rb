39beaeaa06d36d6f898af64eb8870235202e9adb
Raul Ospina <raminka13@hotmail.com>
2024-06-03 18:29:38 -0500

    Update: mini magick resize

--- app/models/reimbursement.rb
+++ app/models/reimbursement.rb
@@ -1,5 +1,6 @@
 class Reimbursement < ApplicationRecord
-  require "rmagick"
+  #require "rmagick"
+  require "mini_magick"
 
   has_one_attached :receipt
 
@@ -7,30 +8,32 @@ class Reimbursement < ApplicationRecord
   validates :last_name, presence: true
   validates :email, presence: true
   validates :entry_date, presence: true
-  validates :amount, presence: true
+  validates :amount, presence: true, numericality: true
   validates :preferred_payment, presence: true
   validates :receipt, content_type: ['image/png', 'image/gif', 'image/jpeg', 'image/jpg', 'image/heic'],
   size: { less_than: 2.megabytes, message: 'File size must be less than 2MB' }
 
-  before_save :resize_image, if: -> { receipt.attached? }
+  #before_save :resize_image, if: -> { receipt.attached? }
 
   private
 
   def resize_image
     max_dimension = 800 # Adjust the maximum dimension as needed
-    # Create a MiniMagick::Image directly from the binary data
-    image = Magick::Image::read(receipt.download).first
-    image.resize_to_fill "#{max_dimension}x#{max_dimension}>"
+    logger.info(:receipt)
 
-    Create a new ActiveStorage::Blob with the resized image data
-    resized_image_blob = ActiveStorage::Blob.create_after_upload!(
+    # Create a MiniMagick::Image directly from the binary data
+    image = MiniMagick::Image.open(receipt[:tempfile].to_s)
+    image.resize "#{max_dimension}x#{max_dimension}>"
+
+    # Create a new ActiveStorage::Blob with the resized image data
+    resized_image_blob = ActiveStorage::Blob.create_and_upload!(
       io: StringIO.new(image.to_blob),
       filename: receipt.filename,
       content_type: image.mime_type
-      )
+    )
 
     # Replace the original attachment with the resized image
     receipt.attach(resized_image_blob)
-    end
+  end
 
 end
